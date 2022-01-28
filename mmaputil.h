957abd7493f3f43c831c4e6107d52160d95da38a
Scheliux <1valaki1asd@gmail.com>
2022-01-28 17:27:22 +0100

    Initial commit

--- mmaputil.h
+++ mmaputil.h
@@ -4,24 +4,18 @@
 #include <map>
 
 template <class Key, class T, class Compare = std::less<Key> >
-class multimap_util
-{
+class multimap_util {
 private:
 	std::multimap<Key, T, Compare> &_map;
 
 public:
 	multimap_util (std::multimap<Key, T, Compare> &m) : _map(m) { }
 
-	multimap_util& erase(T value)
-	{
-		for (typename std::multimap<Key, T, Compare>::iterator it = _map.begin(); it != _map.end();)
-		{
-			if (it->second == value)
-			{
+	multimap_util& erase(T value) {
+		for (typename std::multimap<Key, T, Compare>::iterator it = _map.begin(); it != _map.end();) {
+			if (it->second == value) {
 				_map.erase(it++);
-			}
-			else
-			{
+			} else {
 				++it;
 			}
 		}
@@ -29,12 +23,9 @@ public:
 		return *this;
 	}
 
-	multimap_util& replace_key(Key i, Key j)
-	{
-		for (typename std::multimap<Key, T, Compare>::iterator it = _map.begin(); it != _map.end(); ++it)
-		{
-			if (it->first == i)
-			{
+	multimap_util& replace_key(Key &i, Key &j) {
+		for (typename std::multimap<Key, T, Compare>::iterator it = _map.begin(); it != _map.end(); ++it) {
+			if (it->first == i) {
 				_map.insert( std::make_pair( j, it->second ) );
 				_map.erase(it);
 				break;
@@ -44,34 +35,25 @@ public:
 		return *this;
 	}
 
-	multimap_util& replace_value(T i, T j)
-	{
-		for (typename std::multimap<Key, T, Compare>::iterator it = _map.begin(); it != _map.end(); ++it)
-		{
+	multimap_util& replace_value(T &i, T &j) {
+		for (typename std::multimap<Key, T, Compare>::iterator it = _map.begin(); it != _map.end(); ++it) {
 			if (it->second == i)
-			{
 				it->second = j;
-			}
 		}
 
 		return *this;
 	}
 
-	multimap_util& swap(Key i, Key j)
+	multimap_util& swap(Key &i, Key &j)
 	{
 		T lhs;
 		T rhs;
 
-		for (typename std::multimap<Key, T, Compare>::reverse_iterator it = _map.rbegin(); it != _map.rend(); ++it)
-		{
+		for (typename std::multimap<Key, T, Compare>::reverse_iterator it = _map.rbegin(); it != _map.rend(); ++it) {
 			if (it->first == i)
-			{
 				lhs = it->second;
-			}
 			else if (it->first == j)
-			{
-				rhs = it->second;	
-			}
+				rhs = it->second;
 		}
 	
 		_map.find(i)->second = rhs;
@@ -79,7 +61,6 @@ public:
 
 		return *this;
 	}
-
 };
 
 #endif
